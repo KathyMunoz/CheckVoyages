0afad626097c73c9261b3edd2095a29960f53033
KathyMunoz <katherine.munoz.moran@gmail.com>
2026-02-12 16:27:26 +0100

    erreur

--- View/view_destinationList.php
+++ View/view_destinationList.php
@@ -10,7 +10,7 @@ class DestinationListView {
             $html .= "<div class='cards-grid' style='display: grid; grid-template-columns: repeat(auto-fit, minmax(200px, 1fr)); gap: 1.5rem; justify-content: center; margin-top: 30px;'>";
             foreach ($destinations as $dest) {
                 $img = !empty($dest['thumbnail']) ? "<img src='images/".$dest['thumbnail']."' alt='".$dest['title']."' style='width: 100%; max-width:150px; height: 160px; object-fit: cover; margin-bottom: 1rem; border-radius: 8px;'>" : "";
-                $html .= "<a href='destination.php?id=".$dest['id_destination']."' class='card card-image' style='border: solid 2px rgb(255 113 12); border-radius: 10px; overflow: hidden; text-align: center; padding: 1rem; text-decoration: none; color: black;'>
+                $html .= "<a href='destination.php?id=".$dest['id_destination']."' class='card card-image'>
                             $img
                             <p class='card-description' style='font-size: 1rem; font-weight: bold;'>".$dest['title']."</p>
                          </a>";
